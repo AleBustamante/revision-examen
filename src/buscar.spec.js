340639a584353595d1f1cb21fdcdab3a1e410969
unknown <adbdc.u@gmail.com>
2024-05-07 10:03:38 -0400

    Busca un proyecto en una lista de un solo proyecto

--- src/buscar.spec.js
+++ src/buscar.spec.js
@@ -6,9 +6,19 @@ describe("Buscar", () => {
     let proyectos = [];
     expect(buscarProyecto("ejercicio1", proyectos)).toEqual("");
   });
+  it("encuentra un poryecto cuando el mismo existe en una lista de 1 proyecto", () => {
+    let proyectos = [];
+    proyectos.push("miUnicoProyecto");
+    expect(buscarProyecto("miUnicoProyecto", proyectos)).toEqual("miUnicoProyecto");
+  });
 });
 
 
 function buscarProyecto(nombre, proyectos) {
-    return "";
+    if (proyectos.length === 0) {
+        return "";
+    }
+    if (proyectos[0] === nombre) {
+        return proyectos[0];
+    }
 }
