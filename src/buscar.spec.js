26fdedae609ebc6aa2262b00ce3b5d5e6e70e988
unknown <adbdc.u@gmail.com>
2024-05-07 10:15:54 -0400

    Refactorizadas las pruebas y funcion buscarProyecto para trabajar con array de resultados

--- src/buscar.spec.js
+++ src/buscar.spec.js
@@ -4,12 +4,14 @@
 describe("Buscar", () => {
   it("no encuentra proyectos cuando no se tiene ninguno en la lista de proyectos", () => {
     let proyectos = [];
-    expect(buscarProyecto("ejercicio1", proyectos)).toEqual("");
+    let listaEsperada = [""];
+    expect(buscarProyecto("ejercicio1", proyectos)).toEqual(listaEsperada);
   });
   it("encuentra un poryecto cuando el mismo existe en una lista de 1 proyecto", () => {
     let proyectos = [];
     proyectos.push("miUnicoProyecto");
-    expect(buscarProyecto("miUnicoProyecto", proyectos)).toEqual("miUnicoProyecto");
+    let listaEsperada = ["miUnicoProyecto"];
+    expect(buscarProyecto("miUnicoProyecto", proyectos)).toEqual(listaEsperada);
   });
   it("encuentra un proyecto cuando el mismo existe en una lista de varios proyectos", () => {
     let proyectos = [];
@@ -17,18 +19,23 @@ describe("Buscar", () => {
     proyectos.push("proyecto2");
     proyectos.push("proyecto3");
     proyectos.push("proyecto4");
-    expect(buscarProyecto("proyecto2", proyectos)).toEqual("proyecto2");
+    let listaEsperada = ["proyecto2"];
+    expect(buscarProyecto("proyecto2", proyectos)).toEqual(listaEsperada);
   });
 });
 
 
 function buscarProyecto(nombre, proyectos) {
+    let encontrado = [];
     if (proyectos.length === 0) {
-        return "";
+        encontrado.push("");
+        return encontrado;
     }
+
     for (const proyecto of proyectos) {
         if (proyecto === nombre) {
-            return proyecto;
+            encontrado.push(proyecto);
+            return encontrado;
         }
-    }  
+    } 
 }
